6fc60f1d7fb956c0d9462e05b34f55ac95ee0d73
FeignClaims <www4112379@163.com>
2024-01-31 22:15:56 +0800

    调整延时刷新的体验

--- docs/_static/no_cache.js
+++ docs/_static/no_cache.js
@@ -1,8 +1,8 @@
-window.addEventListener("load", (event) => {
+window.addEventListener("DOMContentLoaded", (event) => {
   if (window.localStorage) {
     if (!localStorage.getItem("firstLoad")) {
       localStorage["firstLoad"] = true;
-      setTimeout(() => window.location.reload(), 3000);
+      setTimeout(() => window.location.reload(), 1000);
     } else localStorage.removeItem("firstLoad");
   }
 });
